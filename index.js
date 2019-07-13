c47ebf838794a6eb84f6f2d89d2c0f6bb04c1c7b
Carolina Ferreira <carolina.ferreira.16@ucl.ac.uk>
2019-07-13 00:27:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 var name = "Joe"
-var height = 70 
-
-if (name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected the name to be Susan")
-}
+var height = "74"
 
 // Don't worry about this
 module.exports = { name, height
 }
+
